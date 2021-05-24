cd563baa4d211b98f2cd6029a26c51244e3ced00
Garry Joss <73390442+gj0-dev@users.noreply.github.com>
2021-05-24 07:45:00 +0100

    simon game cont.

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,21 +1,32 @@
-gamePattern = [];
-buttonColours = ["red", "blue", "green", "yellow"]
+let gamePattern = [];
+let buttonColours = ["red", "blue", "green", "yellow"]
+
+let userClickedPattern = [];
+
+$(".btn").click(function(){
+
+    let userChosenColour = $(this).attr("id");
+
+    userClickedPattern.push(userChosenColour);
+
+    console.log(userClickedPattern);
+
+})
 
 function nextSequence(){
    let randomNumber = Math.floor(Math.random() * 4 );
-    console.log(randomNumber);
     let randomChosenColour = buttonColours[randomNumber];
-    console.log(randomChosenColour);
+
     gamePattern.push(randomChosenColour);
-    console.log(gamePattern);
+
     $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio('"\\sounds\\" + randomChosenColour + ".mp3"');
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
     audio.play();
 };
 
 nextSequence();
 
-
+$button
 
 
