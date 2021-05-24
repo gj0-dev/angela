3909df649df693e2f56295504fd14575846c5ab8
Garry Joss <73390442+gj0-dev@users.noreply.github.com>
2021-05-24 07:01:17 +0100

    simon game cont

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -8,9 +8,14 @@ function nextSequence(){
     console.log(randomChosenColour);
     gamePattern.push(randomChosenColour);
     console.log(gamePattern);
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio('"\\sounds\\" + randomChosenColour + ".mp3"');
+    audio.play();
 };
 
 nextSequence();
+
 
 
 
