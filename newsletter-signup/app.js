fb5dac84406015012aad146823e7dc1e0e054c6b
Garry Joss <73390442+gj0-dev@users.noreply.github.com>
2021-05-31 07:44:51 +0100

    newsletter project cont.

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -1,16 +1,32 @@
 const express = require('express');
 let bodyParser = require('body-parser')
 const request = require('request');
-const { response } = require('express');
-
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res)=>{
-    res.send("HELLO");
-})
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post('/', (req, res)=>{
+
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+    console.log(firstName);
+    console.log(lastName);
+    console.log(email);
+
+});
 
 
 app.listen(3000, function(){
     console.log("LISTENING ON PORT 3000")
 });
+
+
+// e501cd90d9d6d5da7a36a0981e83ab1d-us6
+// mailchimp api key
