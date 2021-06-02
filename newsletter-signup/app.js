0a5f5318e383845c7a66b898ebfc087895e79613
Garry Joss <73390442+gj0-dev@users.noreply.github.com>
2021-06-02 14:14:28 +0100

    newsletter cont.

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 let bodyParser = require('body-parser')
 const request = require('request');
+const https = require("https");
 
 const app = express();
 
@@ -13,12 +14,41 @@ app.get('/', (req, res)=>{
 
 app.post('/', (req, res)=>{
 
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
-    console.log(firstName);
-    console.log(lastName);
-    console.log(email);
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME:firstName,
+                    LNAME: lastName
+                }
+
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us6.api.mailchimp.com/3.0/lists/f3d950cf0e";
+
+    const options = {
+        method: "POST",
+        auth: "gj0:8b21db907ff79ae20afcf943ebbc6261-us6"
+    }
+
+    const request = https.request(url, options, function (response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
 
 });
 
@@ -27,6 +57,8 @@ app.listen(3000, function(){
     console.log("LISTENING ON PORT 3000")
 });
 
+// Audience ID
+// 8b21db907ff79ae20afcf943ebbc6261-us6
 
-// e501cd90d9d6d5da7a36a0981e83ab1d-us6
-// mailchimp api key
+// List ID
+// f3d950cf0e
