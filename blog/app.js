759b3328affab07114c2d1b7c96b3988c039c0ad
Garry Joss <gj0@outlook.com>
2021-08-12 07:28:48 +0100

    blog project cont

--- blog/app.js
+++ blog/app.js
@@ -15,18 +15,47 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+var posts = [];
+
+app.get("/", function(req, res){
+
+  res.render("home", { startingContent: homeStartingContent,
+    posts: posts });
+
+})
+
+app.get("/contact", function(req, res){
+
+  res.render("contact", { contactContent: contactContent });
+})
+
+app.get("/compose", function(req, res){
+
+  res.render("compose");
+})
+
+app.post("/compose", function(req, res){
+
+  console.log(req.body.postTitle);
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+});
+
+app.get("/posts/:postName", function(req, res){
+  console.log(req.params.postName);
+});
 
 
-
-
-
-
-
-
-
-
-
-
+app.get("/about", function(req, res){
+  
+  res.render("about", { aboutContent: aboutContent });
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
