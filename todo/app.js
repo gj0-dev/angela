7f9a6bcc34ab9aa7ed64373fbe560156abbd218d
Garry Joss <gj0@outlook.com>
2021-08-31 13:46:57 +0100

    completed todolist project

--- todo/app.js
+++ todo/app.js
@@ -12,7 +12,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true});
+mongoose.connect("mongodb+srv://<insertusernameandpw>@cluster0.khuwr.mongodb.net/todolistDB", { useNewUrlParser: true});
+
+//admin-garry<colon>test123
 
 const itemsSchema = {
   name: String
@@ -144,6 +146,12 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+ 
+app.listen(port, function() {
+  console.log("Server started succesfully");
+});   
